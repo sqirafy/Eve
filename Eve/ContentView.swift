971b423ff6b5a71a97f855bdc519226f84708d2f
Siphesihle Mavuso <smavusot@gmail.com>
2026-04-01 19:04:35 +0200

    Initial Commit

---
+++ Eve/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Eve
+//
+//  Created by Siphesihle Mavuso on 2026/04/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Eve/EveApp.swift
@@ -0,0 +1,17 @@
+//
+//  EveApp.swift
+//  Eve
+//
+//  Created by Siphesihle Mavuso on 2026/04/01.
+//
+
+import SwiftUI
+
+@main
+struct EveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
